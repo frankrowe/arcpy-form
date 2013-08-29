91d5610ab30d06da30e9ab8b646d69a0438948f3
Frank Rowe <fsrowe@gmail.com>
2013-08-29 15:49:05 -0400

    form route

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
+
+# Home page (localhost:5000)
 @app.route("/")
-def hello():
-    return "Hello World!"
+def hello(name=None):
+  return render_template('form.html', name=name)
+
+
+
+# Form submit (localhost:5000/submit)
+@app.route('/submit', methods=['POST'])
+def submitForm():
+  value1 = request.form['value1']
+  value2 = request.form['value2']
+
+  # do cool GIS things with form values here
+
+  return render_template('success.html', value1=value1, value2=value2)
+
+
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
